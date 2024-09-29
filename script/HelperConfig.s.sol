dc3417d6ffeb76c6fc1736f9f17abf2899b7eaa4
tlorans <thomaslorans@gmail.com>
2024-09-29 08:55:10 +0200

    we have finished to add the helper config for anvilé

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -21,7 +21,7 @@ contract HelperConfig is Script {
         } else if (block.chainid == 1) {
             activeNetworkConfig = getMainnetEthConfig();
         } else {
-            activeNetworkConfig = getAnvilEthConfig();
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
         }
     }
 
@@ -38,7 +38,11 @@ contract HelperConfig is Script {
         });
         return ethConfig;
     }
-    function getAnvilEthConfig() public returns (NetworkConfig memory) {
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
         // 1. Deploy the mocks
         // 2. Return the mock address
 
